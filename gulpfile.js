15391182e147b7a32fba1c4124ce4e0f30b7ebde
Charles Zamora <charles.andacc@gmail.com>
2016-05-23 20:17:02 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp    = require('gulp');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var concat  = require('gulp-concat');
+
+// default src path
+var JS_SRC_PATH     = 'src/objpath.js';
+// default release path
+var RELEASE_PATH    = 'build/release';
+
+// build task
+gulp.task('build', function() {
+    // require file system
+    var fs = require('fs');
+
+    // read original src
+    fs.createReadStream(JS_SRC_PATH)
+    // pipe then write original src
+    .pipe(fs.createWriteStream(RELEASE_PATH + '/objpath.js'));
+
+    // set the src
+    return gulp.src(JS_SRC_PATH)
+    // set the build name
+    .pipe(concat('objpath.js'))
+    // uglify!
+    .pipe(uglify({ mangle : true }))
+    // rename
+    .pipe(rename({ extname : '.min.js' }))
+    // save to release path
+    .pipe(gulp.dest(RELEASE_PATH));
+});
+
+// default task
+gulp.task('default', function() {
+    gulp.start('build');
+});
